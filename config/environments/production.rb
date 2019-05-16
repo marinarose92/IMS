cfb212dd43ae0bc334ebef5c70a39b532c3946a7
marinarose92 <maffia.marina@gmail.com>
2019-05-16 15:30:17 -0400

    added settings for 2 letter tld

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "IMS_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  #TLD of .us
+  config.action_dispatch.tld_length = 2
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
