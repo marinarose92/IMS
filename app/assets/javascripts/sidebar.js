b377274febd2855cd1a2dea0a83964f7665e4d7a
marinarose92 <maffia.marina@gmail.com>
2019-05-16 16:04:24 -0400

    added closeable sidebar to admin panel

---
+++ app/assets/javascripts/sidebar.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("left-sidebar").style.width = "300px";
+    document.getElementById("orders-container").style.marginLeft = "350px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("left-sidebar").style.width = "0";
+    document.getElementById("orders-container").style.marginLeft = "0";
+  }
