d9d0ea0c6a04dac71bcfd165006a5e53654b151c
marinarose92 <maffia.marina@gmail.com>
2019-06-13 11:50:58 -0400

    modified application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
   def require_login
     unless logged_in?
       flash[:error] = "You must be logged in to access this section."
-      redirect_to sessions#new
+      redirect_to new_session_path
     end
   end
 end
