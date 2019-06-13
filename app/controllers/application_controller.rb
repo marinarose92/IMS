a206e779a078ca04cfb142d77f23f094bf64d5c4
marinarose92 <maffia.marina@gmail.com>
2019-06-13 11:24:41 -0400

    modify authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
+  before_action :require_login
 
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
     else
       @current_user = nil
+    end
+  end
+
+  private
+
+  def logged_in?
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section."
+      redirect_to sessions#new
     end
   end
 end
